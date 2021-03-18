76a130980701b5e4f5239d12f66086039233d2c0
Yolanda Diandari <yolandadiandari@gmail.com>
2021-03-18 21:03:13 +0700

    update tugas 3

--- I0320112_soal1_tugas3.py
+++ I0320112_soal1_tugas3.py
@@ -1,1 +1,24 @@
+list= ['yukuri', 'moris', 'zanet', 'sekar', 'vizal', 'deny', 'zafira', 'bonang', 'riri', 'rana']
+
+#Menampilkan list indeks 4,6,7
+print("Nama Teman indeks 4 6 7 :", list[4], ",", list[6], ",", list[7])
+
+#mengganti nama teman list 3,5,9
+list[2] = "Vincen"
+list[4] = "stepy"
+list[8] = "raysa"
+print(list)
+
+#menambah nama teman
+list.extend(['Sasa', 'Rara'])
+print(list)
+
+#menampilkan nama teman dalam perulangan
+urutanlist = 0
+for data in range (0,12):
+    print(list[urutanlist])
+    urutanlist = urutanlist + 1
+
+#menampilkan panjang list
+print(len(list))
 
--- I0320112_soal2_tugas3.py
+++ I0320112_soal2_tugas3.py
@@ -1,1 +1,20 @@
+dict = {'Nama': ['Yolanda Diandari'],
+        'Hobi': ['Membaca','Menulis','Bernyanyi'],
+        'Sosial media': ['IG : @yolan.dn','Line : yolan.dn','Quora : Gaia Mentari'],
+        'Lagu kesukaan' : ['Hericane','Malibu Nights','All I Want'],
+        'Makanan favorit' : ['Sup ayam mama','Dimsum','Pasta']}
+
+print(dict)
+
+#mengubah salah satu hobi dan sosial media
+dict['Hobi'][2] = ('Menulis')
+dict['Sosial media'][1] = ('Twitter : thinkerbelle')
+
+#menghapus 2 makanan favorit
+del(dict)['Makanan favorit'][1:]
+
+#menambahkan 1 hobi
+dict['Hobi'].append('Kuliner')
+
+print(dict)
 
