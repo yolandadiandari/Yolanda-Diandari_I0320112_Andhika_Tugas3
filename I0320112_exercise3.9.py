adb2c4aab625c29fa5a20099d7353abf9f5aa905
Yolanda Diandari <yolandadiandari@gmail.com>
2021-03-18 16:01:21 +0700

    update tugas 3

--- I0320112_exercise3.9.py
+++ I0320112_exercise3.9.py
@@ -1,1 +1,11 @@
+tup1=(12,34,56)
+tup2=('abc','xyz')
 
+#Aksi seperti dibawah ini tidak bisa dilakukan pada tuple python
+#Karena memang nilai pada tuple python tidak bisa dibubah
+#tup1[0] = 100
+
+#Jadi, buatlah tuple baru sebagai berikut
+
+tup3 = tup1 + tup2
+print(tup3)
